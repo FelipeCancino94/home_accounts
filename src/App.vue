89f66ef835508c1971e5aaccca6f6c7b0a39d6fc
Felipe <jcancino@modyo.com>
2022-12-09 19:20:25 -0500

    delete demo component and add Bootstrap CDN

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Home APP</h1>
+  <factures-page></factures-page>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FacturesPage from './pages/FacturesPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FacturesPage,
   }
 }
 </script>
---
+++ src/pages/FacturesPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Factures is Here!</h1>
+    <p>Hello {{test}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FacturesPage',
+  data() {
+    return {
+      test: true,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
