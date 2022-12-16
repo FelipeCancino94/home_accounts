78fcd5eba5fade0a00c40829b01db34b69b2de5a
Felipe Cancino <f.cancino@novatize.com>
2022-12-16 16:54:40 -0500

    clean format and add pageFactures component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './css/App.css';
 
 // COMPONENTS
 import { Footer } from './components/Footer';
+import { PageFactures } from './pages/PageFactures';
 
 const alertMe = (name) => {
   alert('atention ' + name);
@@ -11,28 +12,7 @@ const alertMe = (name) => {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <section>
-        <article>
-        <div className="card text-center">
-          <div className="card-header">
-            Featured
-          </div>
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a
-              href="#"
-              className="btn btn-primary"
-              onClick={() => { alertMe('Felipe') }}>Go somewhere</a>
-          </div>
-          <div className="card-footer text-muted">
-            2 days ago
-          </div>
-        </div>
-        </article>
-      </section>
+      <PageFactures></PageFactures>
       <Footer></Footer>
     </div>
   );
--- src/pages/PageFactures.js
+++ src/pages/PageFactures.js
@@ -1,80 +1,62 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Cuentas servicios publicos</title>
-        <meta charset="utf-8">
-        <!--Talla escitorio-->
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!--CSS Bootstrap 5.2-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    </head>
-
-    <body>
-        <!-- Contenido de la pagina -->
-        <header>
-            <h1 class="text-center text-info m-3">Sacar cuentas de recibos publicos</h1>
-        </header>
-        <main class="container">
-            <!-- Datos recibo-->
-            <div class="mt-3">
-                <p class="fs-4">Servicio publico</p>
-                <select class="form-select py-2 fs-4" aria-label="Default select example" id="selectorRecibo">
-                    <option selected>Seleccione el servicio publico</option>
-                    <option value="luz">Luz</option>
-                    <option value="gas">Gas</option>
-                    <option value="agua">Agua</option>
-                  </select>
-                  <label class="fs-4 my-3" for="valorRecibo">Ingrese el valor</label>
-                  <div class="input-group">
-                    <input type="text" id="valorRecibo" class="form-control" aria-label="Monto pesos (con tres decimales despues del punto">
-                    <span class="input-group-text">$</span>
-                  </div>
-            </div>
-            <!-- Agregar visitante -->
-            <div class="accordion my-3" id="accordionVisitante">
-                <div class="accordion-item">
-                  <h2 class="accordion-header" id="tituloAcordeon">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseVisitante" aria-expanded="false" aria-controls="collapseVisitante">
-                      Agregar visitante
-                    </button>
-                  </h2>
-                  <div id="collapseVisitante" class="accordion-collapse collapse" aria-labelledby="tituloAcordeon" data-bs-parent="#accordionVisitante">
-                        <div class="accordion-body ">
-                            <p class="fs-5">Apartamento del visitante</p>
-                            <select class="form-select py-1 fs-5" aria-label="Default select Visitante" id="aptoVisitante">
-                                <option selected>Seleccione numero de apto</option>
-                                <option value="301">301 (Propietarios)</option>
-                                <option value="302">302</option>
-                                <option value="201">201</option>
-                                <option value="102">202/102</option>
-                                <option value="101">101</option>
-                                <option value="local">Local</option>
-                            </select>
-                            <div class="input-group my-3 ">
-                                <span class="input-group-text" >Numero de visitantes</span>
-                                <input type="number" class="form-control"  aria-label="numerohuesped" aria-describedby="cantVistantes">
-                            </div>
-                            <div class="input-group my-3">
-                                <span class="input-group-text">Numero de dias estancia</span>
-                                <input type="number" class="form-control" aria-label="numerodias" id="numDiasVistante">
-                            </div>
-                        </div>
-                    </div>
+const PageFactures = () => {
+  return (
+    <div id="page-factures">
+      <h1 className="text-center text-info m-3">Sacar cuentas de recibos publicos</h1>
+      <div className="container">
+        {/* Datos recibo */}
+        <div className="mt-3">
+          <p className="fs-4">Servicio publico</p>
+          <select className="form-select py-2 fs-4" aria-label="Default select example" id="selectorRecibo">
+            <option selected>Seleccione el servicio publico</option>
+            <option value="luz">Luz</option>
+            <option value="gas">Gas</option>
+            <option value="agua">Agua</option>
+          </select>
+          <label className="fs-4 my-3" for="valorRecibo">Ingrese el valor</label>
+          <div className="input-group">
+            <input type="text" id="valorRecibo" className="form-control" aria-label="Monto pesos (con tres decimales despues del punto" />
+            <span className="input-group-text">$</span>
+          </div>
+        </div>
+        {/* Agregar visitante */}
+        <div className="accordion my-3" id="accordionVisitante">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="tituloAcordeon">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseVisitante" aria-expanded="false" aria-controls="collapseVisitante">
+                Agregar visitante
+              </button>
+            </h2>
+            <div id="collapseVisitante" className="accordion-collapse collapse" aria-labelledby="tituloAcordeon" data-bs-parent="#accordionVisitante">
+              <div className="accordion-body ">
+                <p className="fs-5">Apartamento del visitante</p>
+                <select className="form-select py-1 fs-5" aria-label="Default select Visitante" id="aptoVisitante">
+                    <option selected>Seleccione numero de apto</option>
+                    <option value="301">301 (Propietarios)</option>
+                    <option value="302">302</option>
+                    <option value="201">201</option>
+                    <option value="102">202/102</option>
+                    <option value="101">101</option>
+                    <option value="local">Local</option>
+                </select>
+                <div className="input-group my-3 ">
+                    <span className="input-group-text" >Numero de visitantes</span>
+                    <input type="number" className="form-control"  aria-label="numerohuesped" aria-describedby="cantVistantes" />
                 </div>
+                <div className="input-group my-3">
+                  <span className="input-group-text">Numero de dias estancia</span>
+                  <input type="number" className="form-control" aria-label="numerodias" id="numDiasVistante" />
+                </div>
+              </div>
             </div>
-            <!-- boton calculo -->
-            <button type="button" class="btn btn-info text-white" onclick="">Calcular</button>
-            <!-- Resultado valor del recibo -->
-            <div id="contenidoResultado">
-            </div>
-        </main>
+          </div>
+        </div>
+        {/* Boton de calculo */}
+        <button type="button" className="btn btn-info text-white" onclick="">Calcular</button>
+        {/* Resultado valor del recibo */}
+        <div id="contenidoResultado"></div>
+      </div>
+    </div>
+  );
+}
 
-        <!--Script-->
-        <script src=""></script>
-        <!--Script Bootstrap-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
-
-    </body>
-
-</html>
+export { PageFactures };
