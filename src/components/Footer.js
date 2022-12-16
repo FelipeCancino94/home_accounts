29bc74e9bf05bb138f66213ee21207fe826b62d7
Felipe Cancino <f.cancino@novatize.com>
2022-12-16 12:02:22 -0500

    add component footer and clean examples codes

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// CSS
+import '../css/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Home Accounts app Version 0.0.1 © 2022</p>
+    </footer>
+  );
+}
+
+export { Footer };
